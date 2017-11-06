dbba720586d8321636c26d0e5e4005d5daadb070
Sujay66 <sujaym0610@gmail.com>
2017-11-06 12:45:06 -0500

    Adding anagram and excel sheet problem

---
+++ Anagram.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+
+public class Anagram {
+	public static void main(String[] args){
+		System.out.println(isAnagram("anagram","nagaram"));
+	}
+	
+	 public static boolean isAnagram(String s, String t) {
+	  //      if(s.length() != t.length()) return false;
+	        HashMap<Character,Integer> myMap = new HashMap<Character,Integer>();
+	        HashMap<Character,Integer> myMap2 = new HashMap<Character,Integer>();
+	        int i =0;
+	        for(i=0;i<s.length();i++){
+	            if(myMap.containsKey(s.charAt(i))){
+	                myMap.put(s.charAt(i),myMap.get(s.charAt(i))+1);
+	            }else{
+	                myMap.put(s.charAt(i),1);
+	            }
+	        }
+	         for(i=0;i<t.length();i++){
+	            if(myMap2.containsKey(t.charAt(i))){
+	                myMap2.put(t.charAt(i),myMap2.get(t.charAt(i))+1);
+	            }else{
+	                myMap2.put(t.charAt(i),1);
+	            }
+	        }
+	        for(int j=0; j<t.length(); j++){
+	            if(myMap.containsKey(t.charAt(j))){
+	            	System.out.println(myMap.get(t.charAt(j)));
+	            	System.out.println(myMap2.get(t.charAt(j)));
+	            if(myMap.get(t.charAt(j))!= myMap2.get(t.charAt(j))){
+	                return false;
+	            }
+	            }else{
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+
+}
---
+++ ExcelSheetColumnNumber.java
@@ -0,0 +1,17 @@
+
+public class ExcelSheetColumnNumber {
+	
+	public static void main(String[] args){
+		String s = "AA";
+		s = s.toUpperCase();
+        double sum = 0, count = 0;
+        for(int i=s.length()-1;i>=0;i--){
+            double temp = s.charAt(i) - 64;
+            System.out.println(temp);
+            sum = sum + (Math.pow(26.0,count)*temp);
+            count++;
+        }
+        System.out.println((int)sum);
+	}
+
+}
