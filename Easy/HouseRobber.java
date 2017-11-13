9a46cc5d31c5f603cd23cc1501b16ec90568d3b6
Sujay66 <sujaym0610@gmail.com>
2017-11-12 23:35:11 -0500

    Adding house robber problem

---
+++ Easy/HouseRobber.java
@@ -0,0 +1,23 @@
+package Easy;
+
+public class HouseRobber {
+	public static void main(String[] args){
+		int[] nums = {5,1,1,5,10};
+		
+		if(nums==null||nums.length==0)
+	        System.out.println(0);
+	 
+	    if(nums.length==1)
+	        System.out.println(nums[0]);
+	 
+	    int[] dp = new int[nums.length];
+	    dp[0]=nums[0];
+	    dp[1]=Math.max(nums[0], nums[1]);
+	 
+	    for(int i=2; i<nums.length; i++){
+	        dp[i] = Math.max(dp[i-2]+nums[i], dp[i-1]);
+	    }
+	 
+	    System.out.println(dp[nums.length-1]);
+	}
+}
