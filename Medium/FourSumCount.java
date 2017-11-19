87c94138aa67f3b503a82f3a69f1a07c6a4371ad
Sujay66 <sujaym0610@gmail.com>
2017-11-18 23:52:40 -0500

    Adding four array sum equals 0 problem

---
+++ Medium/FourSumCount.java
@@ -0,0 +1,60 @@
+package Medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FourSumCount {
+	public static void main(String[] args){
+		 int[] A = { -1, -1};
+		 int[] B = {-1,1};
+		 int[] C = {-1, 1};
+		 int[] D ={ 1, -1};
+		 
+		 System.out.println(findFourSum(A,B,C,D));
+	}
+	
+	public static int findFourSum(int[] A,int[] B,int[] C,int[] D){
+		
+/*	    Map<Integer, Integer> map = new HashMap<>();
+	    
+	    for(int i=0; i<C.length; i++) {
+	        for(int j=0; j<D.length; j++) {
+	            int sum = C[i] + D[j];
+	            map.put(sum, map.getOrDefault(sum, 0) + 1);
+	        }
+	    }
+	    
+	    int res=0;
+	    for(int i=0; i<A.length; i++) {
+	        for(int j=0; j<B.length; j++) {
+	            res += map.getOrDefault(-1 * (A[i]+B[j]), 0);
+	        }
+	    }
+	    
+	    return res;
+	}*/
+		HashMap<Integer,Integer> myMap = new HashMap<Integer,Integer>();
+		int count = 0;
+		
+		for(int i=0;i<A.length;i++){
+			for(int j=0;j<B.length;j++){
+				int sum = A[i]+B[j];
+				if(myMap.containsKey(sum)){
+					myMap.put(sum, myMap.get(sum)+1);
+				}else{
+					myMap.put(sum, 1);
+				}
+			}
+		}
+		
+		for(int i=0;i<C.length;i++){
+			for(int j=0;j<D.length;j++){
+				int sum = -1 * (C[i]+D[j]);
+				if(myMap.containsKey(sum)){
+					count = count + myMap.get(sum);
+				}
+			}
+		}
+		return count;
+	}
+}
