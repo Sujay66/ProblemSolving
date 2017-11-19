d919deecb31305073a8aef322de240f350df4148
Sujay66 <sujaym0610@gmail.com>
2017-11-19 17:30:07 -0500

    Adding Shuffling Array Problem

---
+++ Arrays/ShuffleArray.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+import java.nio.channels.ShutdownChannelGroupException;
+import java.util.Arrays;
+import java.util.Random;
+
+public class ShuffleArray {
+    public int[] orgNums,tempArray;
+    public ShuffleArray(int[] nums) {
+        orgNums = nums;
+        tempArray = Arrays.copyOf(nums, nums.length);
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return orgNums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+
+        Random random = new Random();
+        for(int i=orgNums.length-1;i>=0;i--){
+            
+         int someNumer = random.nextInt(i+1);
+         int temp = tempArray[someNumer];
+         tempArray[someNumer] = tempArray[i];
+         tempArray[i] = temp;
+        }
+        return tempArray;
+    }
+}
---
+++ Medium/CallShuffleArray.java
@@ -0,0 +1,17 @@
+package Medium;
+
+import java.util.Arrays;
+
+import Arrays.ShuffleArray;
+
+public class CallShuffleArray {
+	public static void main(String[] args){
+		int[] arr = {1,2,3,4,5,6};
+		ShuffleArray myArray = new ShuffleArray(arr);
+		
+		System.out.println(Arrays.toString(myArray.shuffle()));
+		System.out.println(Arrays.toString(myArray.reset()));
+		System.out.println(Arrays.toString(myArray.shuffle()));
+		System.out.println(Arrays.toString(myArray.reset()));
+	}
+}
